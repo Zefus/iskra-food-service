b2b695a8d399695770a25562efa59fd3964022af
Иванов Евгений Константинович <genom990@mail.ru>
2020-01-13 11:18:38 +0700

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: "./components/root.jsx",
+  output: {
+    filename: "build.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
